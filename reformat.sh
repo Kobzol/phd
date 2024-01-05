a182ed8aaf13bb7f22790f13a124e09a9128a1a4
Jakub Beránek <berykubik@gmail.com>
2024-01-05 15:47:01 +0100

    Avoid reformatting generated content

--- reformat.sh
+++ reformat.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-find thesis/chapters -name "*.tex" -exec latexindent -m -o {} {} \; > /dev/null
+find thesis/chapters ! -name 'publications-generated.tex' -name "*.tex" -exec latexindent -m -o {} {} \; > /dev/null
 latexindent -m -o thesis/main.tex thesis/main.tex \; > /dev/null
 find thesis -name indent.log -exec rm {} \;
